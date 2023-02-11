f127dd8518eeed93f01976437ff15452c420e53e
sebifilip <sebifilip@gmail.com>
2023-02-11 11:42:02 +0000

    Added cities to a .txt file.

--- venv/cities_graph.py
+++
@@ -1,8 +0,0 @@
-map = """
-            Reykjavik
-            \ 
-             \ 30
-              \ 
-           5   \ 
-    Dublin------London
-"""
--- venv/main.py
+++ venv/main.py
@@ -1,21 +1,20 @@
 import random
-from cities_graph import map
 
-def shortest_path(cities: dict[str, list[int]]):
-    cities = []
-    cities.append(random.choice(list(european_cities)))
-    cities.append(random.choice(list(european_cities)))
+def load_data(data: str):
+    graph_data = {}
+    with open(data, "r") as reader:
+        for line in reader.readlines():
+            line = line.strip()
+            connection = line.split()
+            graph_data[connection[0], connection[1]] = int(connection[2])
+    return graph_data
+
+
+def shortest_path(cities: dict[list[str], int]):
+    start = random.choice(list(cities))
+    destination = random.choice(list(cities))
+    while start == destination:
+        destination = random.choice(list(cities))
     shortest_path = int(input(f"Find the shortest path between {cities[0]} and {cities[1]}: "))
 
-european_cities = {
-    "Amsterdam": [0, 3, 2, 0, 3, 0, 1, 0, 0, 0],
-    "Berlin": [3, 0, 2, 0, 0, 0, 6, 12, 8],
-    "Copenhagen": [2, 2, 0, 0, 0, 0, 0, 0, 0, 16],
-    "Dublin": [0, 0, 0, 0, 5, 0, 0, 0, 0, 0],
-    "London": [3, 0, 0, 5, 0, 0, 10, 30, 0, 0],
-    "Madrid": [0, 0, 0, 0, 25, 0, 10, 0, 0, 0],
-    "Paris": [1, 6, 0, 0, 10, 10, 0, 0, 12, 0],
-    "Reyjjavik": [0, 0, 0, 0, 30, 0, 0, 0, 0, 0],
-    "Rome": [0, 12, 0, 0, 0, 0, 12, 0, 0],
-    "Warsaw": [0, 8, 16, 0, 0, 0, 0, 0, 0]
-    }
+print(load_data("cities_graph"))
