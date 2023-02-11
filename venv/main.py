d3875f91978f3c567ee72b3e5fc8e2a1e9e4fd87
sebifilip <sebifilip@gmail.com>
2023-02-11 14:52:08 +0000

    Completed gen_matrix function.

--- venv/main.py
+++ venv/main.py
@@ -8,16 +8,35 @@ def load_data(data: str):
             line = line.strip()
             connection = line.split()
             graph_data[connection[0], connection[1]] = int(connection[2])
+            graph_data[connection[1], connection[0]] = int(connection[2])
             pair = [connection[0], connection[1]]
             for c in pair:
                 if c not in cities:
                     cities.append(c)
+
     print(cities)
     print(graph_data)
-    shortest_path(graph_data, cities)
+    gen_matrix(graph_data, cities)
 
 
-def shortest_path(graph_data: dict[list[str], int], cities: list[str]):
+def gen_matrix(graph_data: dict[list[(str, str)], int], cities: list[str]):
+    distance_matrix = {}
+    for c in sorted(cities):
+        distance_matrix[c] = []
+    for c1 in distance_matrix:
+        for c2 in sorted(cities):
+            if c1 != c2 and (c1, c2) in list(graph_data):
+                distance_matrix[c1].append(graph_data[(c1, c2)])
+            else:
+                distance_matrix[c1].append(0)
+    print(distance_matrix)
+
+
+def find_shortest_path(start, end):
+    shortest_paths = []
+
+
+def prompt_user(graph_data: dict[list[(str, str)], int], cities: list[str]):
     start = random.choice(list(cities))
     destination = random.choice(list(cities))
     while start == destination:
