0e14bafd48e4a917612ba851fbce9c0ee3d4aa88
sebifilip <sebifilip@gmail.com>
2023-02-11 21:52:46 +0000

    Finished gen_matrix.

--- venv/main.py
+++ venv/main.py
@@ -12,32 +12,17 @@ def load_data(data: str):
                 graph_data[connection[0]] = (float(connection[1]), float(connection[2]))
             i += 1
     print(graph_data)
-    gen_matrix(graph_data)
 
 
-def gen_matrix(graph_data: dict[list[(str, str)], int], cities: list[str]):
-    distance_matrix = {}
-    for c in sorted(cities):
-        distance_matrix[c] = []
-    for c1 in distance_matrix:
-        for c2 in sorted(cities):
-            if c1 != c2 and (c1, c2) in list(graph_data):
-                distance_matrix[c1].append(graph_data[(c1, c2)])
-            else:
-                distance_matrix[c1].append(0)
-    print(distance_matrix)
-
-
-def find_shortest_path(start, end):
-    shortest_paths = []
-
-
-def prompt_user(graph_data: dict[list[(str, str)], int], cities: list[str]):
+def prompt_user(cities: dict[list[(str, str)], int]):
     start = random.choice(list(cities))
     destination = random.choice(list(cities))
     while start == destination:
         destination = random.choice(list(cities))
-    shortest_path = int(input(f"Find the shortest path between {start} and {destination}: "))
+    shortest_path = sqrt(abs(cities[start][0]^2) + abs(cities[start[1]^2])) + sqrt(abs(cities[destination][0]^2) + abs(cities[destination[1]^2]))
+    shortest_input = int(input(f"Find the shortest path between {start} and {destination}: "))
+    if shortest_input == shortest_path:
+        print("Correct!")
 
 
 print(load_data("cities_coords.txt"))
