7a749cc8c4070072c19454d6b04e04b27e161852
sebifilip <sebifilip@gmail.com>
2023-02-11 23:07:05 +0000

    Put the formula for finding the distance between two cities.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 def load_data(data: str):
     graph_data = {}
     i = 0
@@ -11,17 +10,26 @@ def load_data(data: str):
                 connection = line.split(",")
                 graph_data[connection[0]] = (float(connection[1]), float(connection[2]))
             i += 1
-    print(graph_data)
+    return graph_data
 
 
-def prompt_user(cities: dict[list[(str, str)], int]):
-    start = random.choice(list(cities))
-    destination = random.choice(list(cities))
-    while start == destination:
+def prompt_user(cities: dict[str, (float, float)]):
+    while True:
+        start = random.choice(list(cities))
         destination = random.choice(list(cities))
+        while start == destination:
+            destination = random.choice(list(cities))
+        start_coords: (float, float) = input(f"Please enter the coordinates for {start} in the format (x, y): ")
+        end_coords: (float, float) = input(f"Please enter the coordinates for {destination} in the format (x, y): ")
+        if start_coords == cities[start] and end_coords == cities[destination]:
+            distance = (abs(start[0] - destination[0])**2 + abs(start[0] - destination[1])**2)**0.5
+            print(f"The shortest distance between {start} and {destination} is {distance}")
+            break
+        
 
 
 print(load_data("cities_coords.txt"))
+prompt_user(load_data("cities_coords.txt"))
 
 
 # References:
