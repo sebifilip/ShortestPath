3943de81058831a343cede2fc9c9b1d481d0ea6d
sebifilip <sebifilip@gmail.com>
2023-02-12 18:36:40 +0000

    Finished filling nodes and edges for coords_graph.txt.

--- main.py
+++ main.py
@@ -2,49 +2,22 @@ import random
 from time import time
 
 
-def load_data(data: str):
-    cities_data = {}
-    i = 0
-    with open(data, "r") as reader:
+def load_coordinates(coord_file: str) -> list[(float, float)]:
+    result: list[(float, float)] = []
+    with open(coord_file, "r") as reader:
         for line in reader.readlines():
-            if i > 0:
-                line = line.strip()
-                connection = line.split(",")
-                cities_data[connection[0]] = (float(connection[1]), float(connection[2]))
-            i += 1
-    return cities_data
+            line = line.strip()
+            connection = line.split(",")
+            result.append((float(connection[1]), float(connection[2])))
+    return result
 
 
-def prompt_user(cities: dict[str, (float, float)], start, destination):
-    while True:
-        start_x = float(input(f"Please enter the x coordinate for {start}: "))
-        start_y = float(input(f"Please enter the y coordinate for {start}: "))
-        end_x = float(input(f"Please enter the x coordinate for {destination}: "))
-        end_y = float(input(f"Please enter the y coordinate for {destination}: "))
-        start_coords = (start_x, start_y)
-        end_coords = (end_x, end_y)
-        if start_coords == cities[start] and end_coords == cities[destination]:
-            distance = ((start_x - end_x)**2 + (start_y - end_y)**2)**0.5
-            print(f"The shortest distance between {start} and {destination} is {distance}")
-            break
-        else:
-            print("Incorrect cooridnates!")
-
-
-def test_run(start, end):
-    start_time = time()
-    cities_dict = load_data("cities_coords.txt")
-    print(cities_dict)
-    prompt_user(cities_dict, start, end)
-    end_time = time()
-    elapsed = end_time - start_time
-    print(f"Elapsed time: {elapsed} seconds")
+def load_graph():
+    pass
 
 
 if __name__ == "__main__":
-    test_run("Bucharest", "Zurich")
-    test_run("London", "Astana")
-
+    pass
 
 # References:
 # Lect%2002.pdf
