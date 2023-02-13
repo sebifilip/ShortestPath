a810e30cc8f88a105d0fa3aea82e433125cd416a
sebifilip <sebifilip@gmail.com>
2023-02-13 21:00:33 +0000

    Implemented a function to load edges and their distances.

--- main.py
+++ main.py
@@ -23,8 +23,26 @@ def load_graph(graph_file: str) -> dict[str, list[str]]:
     return result
 
 
-print(load_coordinates("cities_coords.txt"))
-print(load_graph("cities_graph.txt"))
+def load_distances(c: str, g: str):
+    coords = load_coordinates(c)
+    graph = load_graph(g)
+    cities_coords: dict[str, (float, float)] = {}
+    distances: dict[(str, str), float] = {}
+    for i in range(0, len(coords)):
+        cities_coords[list(graph)[i]] = coords[i]
+    for c1 in graph:
+        for c2 in graph[c1]:
+            distances[(c1, c2)] = 0
+    for pair in distances:
+        distances[pair] = ((cities_coords[pair[0][0]] + cities_coords[pair[0][1]])**2 + (cities_coords[pair[1][0]] + cities_coords[pair[1][1]])**2)**0.5
+    return distances
+
+
+def get_shortest_distance(cities_graph, start, destination):
+    connections = [start]
+
+
+print(load_distances("cities_coords.txt", "cities_graph.txt"))
 
 
 if __name__ == "__main__":
